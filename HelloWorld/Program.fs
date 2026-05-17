d66dfc5c4cc8fe0b3c0a5676a528cfccb353ca7b
kitakou0313 <kitakou0313@gmail.com>
2026-05-17 09:24:28 +0900

    add: add memo about functional programing

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -1,1 +1,13 @@
-﻿printfn "Hello, world"
+﻿// 関数の定義
+
+let add1 x= x + 1
+let add2 x y = x + y
+
+// returnは明示的に書かなくていい
+let squarePlusOne x = 
+    let square = x * x
+    square + 1
+
+let areEqual x y =
+    x = y
+printfn "Hello, world"
