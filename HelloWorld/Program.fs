74863ddd6aa738b4a96620ed6be12eaba0c61768
kitakou0313 <kitakou0313@gmail.com>
2026-05-18 07:59:47 +0900

    add: add memo about type composition

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -10,4 +10,52 @@ let squarePlusOne x =
 
 let areEqual x y =
     x = y
-printfn "Hello, world"
+
+type AppleVariety = int
+type BananaVariety = int
+type CherryVariety = int
+
+// AND型の定義（直積型）
+type FruitSalad = {
+    Apple: AppleVariety
+    Banana: BananaVariety
+    Cherry: CherryVariety
+}
+
+// OR型の定義（直和型）
+type FruitSnack1 = 
+    | Apple of AppleVariety
+    | Banana of BananaVariety
+    | Cherry of CherryVariety
+type FruitSnack2 = 
+    | AppleVariety
+    | BananaVariety
+    | CherryVariety
+
+// 単純型
+type ProductCode = ProductCode of string
+
+// 型の定義、値の定義
+//// レコード型
+// typeで型定義
+type Person = {First: string; Last: string}
+//letで値定義
+// レコード型の値を定義
+let aPerson = {First = "test1"; Last = "test2"}
+// パターンマッチで値を分解
+let {First=first;Last=last} = aPerson
+let first2 = aPerson.First
+let last2 = aPerson.Last
+
+//// 判別共用体
+// ケースラベルを介して定義
+type OrderQuantity = 
+    | UnitQuantity of int
+    | KilogramQuantity of float
+let anOrderQtyInUnits = UnitQuantity 10
+let anOrderQtyInKg = KilogramQuantity 2.5
+// 選択型の分解
+let printQuantity aOrderQty = 
+    match aOrderQty with
+    | UnitQuantity uQty -> printfn "Units: %d" uQty
+    | KilogramQuantity kgQty -> printfn "Kilograms: %f" kgQty
