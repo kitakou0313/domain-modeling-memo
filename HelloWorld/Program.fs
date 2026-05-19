8d585aaf237d1e07eb64c46e8e61a029d27488e1
kitakou0313 <kitakou0313@gmail.com>
2026-05-20 08:20:45 +0900

    add: add memo about collection

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -94,7 +94,37 @@ type PersonalInfo = {
 }
 
 // エラー
-type PayInvoiceError = UnpaidInvoice -> Payment -> Result<int, string>
+// type PayInvoiceError = UnpaidInvoice -> Payment -> Result<int, string>
 
 // 値を返さない
 type SaveCustomer = PersonalInfo -> unit
+
+// コレクションを用いたモデリング
+type Order = {
+    OrderId: int
+    Lines: string list
+}
+let aList = [1;2;3]
+// cons演算子を使って配列
+let aList2 = 0 :: aList
+// let alist= aList :: 4 // これはエラー
+
+// パターンを利用してリストを分解してその中の要素にアクセス
+// リストリテラルを使ったパターンマッチ
+let pringList1 aList = 
+    match aList with
+    | [] -> printfn "Empty list"
+    | [x] -> printfn "Single element: %d" x
+    | [x;y] -> printfn "Two elements: %d, %d" x y
+    | longerList -> printfn "There are more than two elements: %A" longerList
+
+// cons演算子を使ってのパターンマッチを利用
+let pringList2 aList = 
+    match aList with
+    | [] -> printfn "Empty list"
+    | first :: rest -> printfn "First element: %d, Rest of the list: %A" first rest
+
+pringList1 []
+pringList1 [1]
+pringList1 [2;3]
+pringList1 [4;5;6]
