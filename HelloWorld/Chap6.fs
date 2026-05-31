2555bf0f7aec22b580b4cfc49a20c21e648b7ef2
kitakou0313 <kitakou0313@gmail.com>
2026-05-31 13:05:50 +0900

    add: add memo about chap6

--- HelloWorld/Chap6.fs
+++ HelloWorld/Chap6.fs
@@ -9,3 +9,9 @@ module UnitQuantity =
             Error "Unit Quantity must be between 1 and 1000"
         else
             Ok(UnitQuantity qty)
+// moduleの中に型を定義しないのは、
+// - module内でmoduleと同名の型を定義できない
+// - {module名}.{コンストラクタ関数}だと意味的に不適切なため
+// - {型名}.{適当な名前}だと型名の参照時に意味的に不適切なため
+// memo:型でのビジネスルールの表現の意味論的なわかりやすさを重視しているためにこのような実装になっていそう
+// Javaのように型のコンストラクタの挙動を変更できない
